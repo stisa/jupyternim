e11b017548ca33220571b936f623b574eaae2043
Silvio T <stisa4@gmail.com>
2016-11-13 12:47:46 +0100

    add temporary highlighting for nim notebooks

--- INim.nimble
+++ INim.nimble
@@ -11,21 +11,12 @@ requires "nim >= 0.14.2"
 requires "zmq"
 requires "hmac"
 requires "nimSHA2"
-# Optional: graph
 
-##  Write the spec... 
-#  writeFile("nim-spec/kernel.json", r"""{
-# "argv": ["<nimbledir>\inim\\nimkernel",  "{connection_file}"],
-#  "display_name": "nim - pure",
-#  "language": "nim",
-#  "file_extension": ".nim"
-#}""")
 bin = @["nimkernel"]
 
 after install:
   echo "Saving kernel spec"
-  echo thisDir() & "nimkernel"
-  exec(r"nim c --hints:off --d:release kernelspec.nim")
+  exec(r"nim c -r --hints:off --d:release kernelspec.nim")
   
 after build:
   echo "Saving kernel spec"
@@ -33,14 +24,5 @@ after build:
   #  echo thisDir() & r"\nimkernel"
   #else:
   #  echo thisDir() & "/nimkernel"
-  exec(r"nim c -d:debugBuild kernelspec.nim")
-
-task setup, "Setup the Kernel":
-  echo "Saving kernel spec"
-  exec(r"nim c kernelspec.nim")
-  echo "Building Kernel"
-  exec(r"nim c --threads:on nimkernel.nim") # compile kernel
-  exec(r"jupyter-kernelspec install nim-spec --user") # install the spec
-
-task buildkernel, "Build the Kernel":
-  exec(r"nim c --threads:on nimkernel.nim") # compile kernel
+  exec(r"nim c -r --hints:off -d:debugBuild kernelspec.nim")
+  
--- kernelspec.nim
+++ kernelspec.nim
@@ -1,5 +1,5 @@
 static:
-  from os import getHomeDir,walkDir,`/`,PathComponent,execShellCmd,parentDir
+  from os import getHomeDir,walkDir,`/`,PathComponent,execShellCmd,parentDir,existsFile,existsDir,createDir
   from strutils import contains
   import json
 
@@ -11,10 +11,24 @@ static:
       for s in walkDir(nimblePkgsDir):
         if s.kind == pcDir and s.path.contains("INim"): return s.path
 
+  # Save the kernel spec
   let kernelspec = %*{
     "argv": [getPkgDir() / "nimkernel",  "{connection_file}"],
     "display_name": "Nim",
     "language": "nim",
     "file_extension": ".nim" }
+  
   writeFile(getPkgDir()/"nim-spec"/"kernel.json", $kernelspec)
   echo staticExec(r"jupyter-kernelspec install " & getPkgDir() / "nim-spec" & " --user") # install the spec
+
+# Append custom js
+var custompath = getHomeDir()/".jupyter"/"custom"/"custom.js"
+createDir(parentDir(custompath)) # create the dirs we need to store custom.js
+proc appendCustom(f:string)=
+  var f = open(getHomeDir()/".jupyter"/"custom"/"custom.js",fmAppend)
+  
+  f.write(readFile(getPkgDir()/"nim-mode"/"custom.js"))
+  f.close()
+
+if existsDir(parentDir(custompath)): appendCustom(custompath)
+else: echo "Could not install highlighting, refer to github.com/stisa/INim for instructions to manually do this"
