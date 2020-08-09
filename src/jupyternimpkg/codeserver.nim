5a956e9b187f0b342dc94b6fbf279bca1d4c94cf
stisa <silvio@tisato.me>
2020-08-09 18:56:59 +0200

    fix hcr with multiple open kernels

--- src/jupyternimpkg/codeserver.nim
+++ src/jupyternimpkg/codeserver.nim
@@ -2,12 +2,12 @@
 
 import hotcodereloading, os, strutils
 
-import codecells #the code to be run
+import $1
 
-template debug(str: varargs[string, `$`]) =
+template debug(str: varargs[string, `$$`]) =
   when not defined(release):
     let inst = instantiationinfo()
-    echo "[" & $inst.filename & ":" & $inst.line & "] ", str.join(" ")
+    echo "[" & $$inst.filename & ":" & $$inst.line & "] ", str.join(" ")
 
 proc codeserver() =
   # we start in paused state, then when we get run we exec once and then go back to paused
